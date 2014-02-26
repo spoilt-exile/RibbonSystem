c81c0310e496ba8aedd4b0a59ceb2adbe4003ae1
Stanislav Nepochatov <spoilt.exile@gmail.com>
2014-02-26 23:53:01 +0200

    Implement Permission entity;

--- src/java/org/ribbon/enteties/Permission.java
+++ src/java/org/ribbon/enteties/Permission.java
@@ -24,4 +24,207 @@ package org.ribbon.enteties;
  */
 public class Permission {
     
+    /**
+     * Id of user entry.
+     */
+    private int id = -1;
+    
+    /**
+     * ID of referenced directory.
+     */
+    private int dirId;
+    
+    /**
+     * Group permission flag.
+     */
+    private Boolean groupPerm;
+    
+    /**
+     * ALL service group permission flag. 
+     * Should be used for wide access specification. 
+     * <b>WARNING:</b> may be <code>null</code>.
+     * <b>WARNING:</b> <code>groupPerm</code> must be enabled to use this flag.
+     */
+    private Boolean allPerm;
+    
+    /**
+     * User ID reference. 
+     * Should be used in condition to <code>groupPerm</code>
+     * <b>WARNING:</b> may be <code>null</code>.
+     * @see #groupPerm
+     */
+    private int userId;
+    
+    /**
+     * Group ID reference. 
+     * Should be used in condition to <code>groupPerm</code>
+     * <b>WARNING:</b> may be <code>null</code>.
+     * @see #groupPerm
+     */
+    private int groupId;
+    
+    /**
+     * Allow to specified user/group/all to read messages in referenced directory.
+     */
+    private Boolean mayRead;
+    
+    /**
+     * Allow to specified user/group/all to post messages to referenced directory.
+     */
+    private Boolean mayPost;
+    
+    /**
+     * Allow to specified user/group/all to admin messages in referenced directory. 
+     * User or group with admin permission can edit or delete messages of other users.
+     */
+    private Boolean mayAdmin;
+    
+    /**
+     * Get id of this entry. Returns -1 if entity wasn't saved.
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set new id for this entry. This will work only for one time.
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        if (this.id == -1) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * Get id of referenced dir.
+     * @return id of directory;
+     */
+    public int getDirId() {
+        return dirId;
+    }
+
+    /**
+     * Set id of referenced directory.
+     * @param dirId the dirId to set
+     */
+    public void setDirId(int dirId) {
+        this.dirId = dirId;
+    }
+
+    /**
+     * Get group permission flag. This flag 
+     * displays which field should be used during access
+     * checking.
+     * @return the groupPerm flag;
+     */
+    public Boolean getGroupPerm() {
+        return groupPerm;
+    }
+
+    /**
+     * Set group permission flag.
+     * @param groupPerm the groupPerm to set
+     */
+    public void setGroupPerm(Boolean groupPerm) {
+        this.groupPerm = groupPerm;
+    }
+
+    /**
+     * Get flag for ALL service group permission.
+     * @return the flag which displays if permission is for ALL group;
+     */
+    public Boolean getAllPerm() {
+        return allPerm;
+    }
+
+    /**
+     * Get flag for ALL service group permission.
+     * @param allPerm the allPerm to set
+     */
+    public void setAllPerm(Boolean allPerm) {
+        this.allPerm = allPerm;
+    }
+
+    /**
+     * Get referenced user id.
+     * @return id or null if permission for group;
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Set referenced user id.
+     * @param userId the id to set;
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Get referenced group id.
+     * @return id or null if permission for user;
+     */
+    public int getGroupId() {
+        return groupId;
+    }
+
+    /**
+     * Set referenced group id.
+     * @param groupId the id to set;
+     */
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    /**
+     * Get read permission for specified user/group on current dir.
+     * @return read permission flag;
+     */
+    public Boolean getMayRead() {
+        return mayRead;
+    }
+
+    /**
+     * Set read permission flag.
+     * @param mayRead the flag to set;
+     */
+    public void setMayRead(Boolean mayRead) {
+        this.mayRead = mayRead;
+    }
+
+    /**
+     * Get post permission for specified user/group on current dir.
+     * @return post permission flag;
+     */
+    public Boolean getMayPost() {
+        return mayPost;
+    }
+
+    /**
+     * Set post permission flag.
+     * @param mayPost the flag to set;
+     */
+    public void setMayPost(Boolean mayPost) {
+        this.mayPost = mayPost;
+    }
+
+    /**
+     * Get admin permission for specified user/group on current dir.
+     * @return admin permission flag;
+     */
+    public Boolean getMayAdmin() {
+        return mayAdmin;
+    }
+
+    /**
+     * Set admin permission flag.
+     * @param mayAdmin the flag to set;
+     */
+    public void setMayAdmin(Boolean mayAdmin) {
+        this.mayAdmin = mayAdmin;
+    }
+    
 }
